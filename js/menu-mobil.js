b275c53f40c563d6e9e0ae453228e73d406b9562
Cejas Matias <wallace223@hotmail.com>
2021-09-25 20:59:43 -0300

    scroll mod

--- js/menu-mobil.js
+++ js/menu-mobil.js
@@ -3,6 +3,7 @@
     let openMenu = document.querySelector('#openMenu');
     let closeMenu = document.querySelector('#closeMenu');
     let menuPantalla = document.querySelector('.menuPantalla');
+    let body = document.querySelector('#body');
 
 
     openMenu.addEventListener('click', showMenu);
@@ -11,9 +12,11 @@
 
     function showMenu() {
         menuPantalla.style.display = 'flex';
+        body.style.overflow = 'hidden';
     }
 
     function hideMenu() {
         menuPantalla.style.display = 'none';
+        body.style.overflow = 'auto';
     }
 // }())
