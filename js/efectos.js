c538016c0a4c6a7e4cba1c5ab008ea80456c112e
unknown <wallace223@hotmail.com>
2021-10-04 19:03:47 -0300

    scroll terminado

--- js/efectos.js
+++ js/efectos.js
@@ -1,37 +1,40 @@
 $(document).ready(function(){
+    let alturaDelViewport = window.innerHeight;
 
+    
 
-
-    var android = $('#titulo-android').offset().top,
+    
+    let android = $('#titulo-android').offset().top,
         javascript = $('#titulo-javascript').offset().top,
         css = $('#titulo-css').offset().top,
         sql = $('#titulo-sql').offset().top;
 
     $('#card-android').on('click', function(e){
         e.preventDefault();
+        
         $('html, body').animate({
-            scrollTop: android - 100
+            scrollTop: android - 50
         }, 500)
     });
 
     $('#card-javascript').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: javascript - 150
+            scrollTop: javascript - 50
         }, 500)
     });
 
     $('#card-css').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: css - 150
+            scrollTop: css - 50
         }, 500)
     });
 
     $('#card-sql').on('click', function(e){
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: sql
+            scrollTop: sql - 50
         }, 500)
     });
 
